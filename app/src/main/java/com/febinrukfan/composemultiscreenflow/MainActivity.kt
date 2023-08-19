293b8a8cda5e722028083ab29549d9baf14a4138
Febin Rukfan <32138498+FebinRukfan@users.noreply.github.com>
2023-08-18 21:37:18 -0400

    INITIAL commit

---
+++ app/src/main/java/com/febinrukfan/composemultiscreenflow/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.febinrukfan.composemultiscreenflow
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.febinrukfan.composemultiscreenflow.ui.theme.ComposeMultiScreenFlowTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeMultiScreenFlowTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ComposeMultiScreenFlowTheme {
+        Greeting("Android")
+    }
+}
